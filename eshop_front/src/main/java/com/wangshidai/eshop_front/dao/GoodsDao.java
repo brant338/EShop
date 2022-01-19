cd6a8eb7911e718a3819d578a8a5e93299da8837
brant来 <3207477062@qq.com>
2022-01-20 00:36:36 +0800

    提交

--- eshop_front/src/main/java/com/wangshidai/eshop_front/dao/GoodsDao.java
+++
@@ -1,9 +0,0 @@
-package com.wangshidai.eshop_front.dao;
-
-import com.wangshidai.eshop_front.pojo.TypeInfo;
-
-import java.util.List;
-
-public interface GoodsDao {
-    List<TypeInfo> findGoodType(Integer parent_id);
-}
--- eshop_front/src/main/java/com/wangshidai/eshop_front/service/GoodsService.java
+++
@@ -1,9 +0,0 @@
-package com.wangshidai.eshop_front.service;
-
-import com.wangshidai.eshop_front.pojo.TypeInfo;
-
-import java.util.List;
-
-public interface GoodsService {
-    List<TypeInfo> findGoodType(Integer parent_id);
-}
